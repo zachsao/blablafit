bc2049f723bbc5c669b1d042a6763a7c8da7d6a0
zacharia <zakaria.sao@gmail.com>
2018-11-17 21:34:24 +0000

    Navigation UI components
    change fragment navigation with the navigation UI. Added up button

--- app/src/main/java/com/example/fsudouest/blablafit/AccueilFragment.java
+++ app/src/main/java/com/example/fsudouest/blablafit/AccueilFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.navigation.Navigation;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +27,25 @@ public class AccueilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return  inflater.inflate(R.layout.fragment_accueil, container, false);
+        View rootView =   inflater.inflate(R.layout.fragment_accueil, container, false);
+
+        Button nouvelle_seance = rootView.findViewById(R.id.programmer_button);
+        Button trouver_seance = rootView.findViewById(R.id.button_trouver);
+        nouvelle_seance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_accueilFragment_to_nouvelleSeanceFragment);
+            }
+        });
+
+        trouver_seance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_accueilFragment_to_trouverUneSeanceFragment);
+            }
+        });
+
+        return rootView;
 
     }
 
--- app/src/main/java/com/example/fsudouest/blablafit/MainActivity.java
+++ app/src/main/java/com/example/fsudouest/blablafit/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.fsudouest.blablafit;
 
 
-import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TimePicker;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import static androidx.navigation.Navigation.findNavController;
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
+import static androidx.navigation.ui.NavigationUI.setupActionBarWithNavController;
+
 
 public class MainActivity extends AppCompatActivity{
 
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavController navController = findNavController(this, R.id.myNavHostFragment);
 
         SharedPreferences preferences=getSharedPreferences("My prefs",0);
         boolean logged_in = preferences.getBoolean("logged_in",false);
@@ -36,76 +37,21 @@ public class MainActivity extends AppCompatActivity{
             finish();
         }
 
-        toolbar = getSupportActionBar();
+        //toolbar = getSupportActionBar();
+        setupActionBarWithNavController(this,navController);
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, new AccueilFragment())
-                .commit();
-
-        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        final BottomNavigationView navigation =findViewById(R.id.bottom_navigation);
         BottomNavigationViewHelper.removeShiftMode(navigation);
-        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.navigation_accueil:
-                        AccueilFragment accueil = new AccueilFragment();
-                        openFragment(accueil);
-                        toolbar.setTitle(getString(R.string.app_name));
-                        break;
-                    case R.id.navigation_seances:
-                        SeancesFragment seances = new SeancesFragment();
-                        openFragment(seances);
-                        toolbar.setTitle(item.getTitle());
-                        break;
-                    case R.id.navigation_messages:
-                        MessagesFragment messages = new MessagesFragment();
-                        openFragment(messages);
-                        toolbar.setTitle(item.getTitle());
-                        break;
-                    case R.id.navigation_profil:
-                        MyProfileFragment profil = new MyProfileFragment();
-                        openFragment(profil);
-                        toolbar.setTitle(item.getTitle());
-                        break;
-                }
-                item.setChecked(true);
-
-
-                return false;
-            }
-        });
+        setupWithNavController(navigation,navController);
 
         CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigation.getLayoutParams();
         layoutParams.setBehavior(new BottomNavigationViewBehavior());
 
-
-    }
-
-    public void openFragment(Fragment fragment){
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, fragment)
-                .commit();
-    }
-
-    public void programmerSeance(View v){
-        openFragment(new NouvelleSeanceFragment());
-        toolbar.setTitle("Programmer ma séance");
-    }
-
-    public void TrouverSeance(View v){
-        openFragment(new TrouverUneSeanceFragment());
-        toolbar.setTitle("Trouver une séance");
-    }
-
-    public void EtapeSuivante(View v){
-        openFragment(new NouvelleSeanceFragment());
-        toolbar.setTitle("Programmer ma séance");
     }
 
 
-
-
-
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        return findNavController(this, R.id.myNavHostFragment).navigateUp();
+    }
 }
