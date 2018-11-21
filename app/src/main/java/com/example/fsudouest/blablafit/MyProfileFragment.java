2d658a078bd669fb4479ca9a1784d71272576c49
zacharia <zakaria.sao@gmail.com>
2018-11-21 23:08:40 +0000

    profile photo update
    Added a dialog before launching the image picker intent

--- app/src/main/java/com/example/fsudouest/blablafit/MyProfileFragment.java
+++ app/src/main/java/com/example/fsudouest/blablafit/MyProfileFragment.java
@@ -1,18 +1,24 @@
 package com.example.fsudouest.blablafit;
 
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.AuthUI;
@@ -80,10 +86,7 @@ public class MyProfileFragment extends Fragment {
         profile_pic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+                createPhotoUpdateDialog();
             }
         });
 
@@ -95,6 +98,37 @@ public class MyProfileFragment extends Fragment {
             }
         });
         return rootview;
+    }
+
+
+    private void chooseImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+    }
+
+    private void createPhotoUpdateDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle(getString(R.string.update_profile_dialog_title));
+        builder.setMessage(getString(R.string.update_photo_dialog_message));
+        // Add the button
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                chooseImageFromGallery();
+            }
+        });
+        builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //
+            }
+        });
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
@@ -111,7 +145,6 @@ public class MyProfileFragment extends Fragment {
                     if (!task.isSuccessful()) {
                         throw task.getException();
                     }
-
                     // Continue with the task to get the download URL
                     return photoRef.getDownloadUrl();
                 }
@@ -125,8 +158,7 @@ public class MyProfileFragment extends Fragment {
                                 .build();
                         user.updateProfile(photoUpdate);
                     } else {
-                        // Handle failures
-                        // ...
+                        Toast.makeText(getActivity(),"Upload Failed - please try again",Toast.LENGTH_SHORT).show();
                     }
                 }
             });
