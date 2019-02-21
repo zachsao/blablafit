4ac0945120b5e97af884c3fea3830624547a368d
zacharia <zakaria.sao@gmail.com>
2019-02-21 16:35:00 +0100

    dataBinding in MessagesFragment

--- app/src/main/java/com/example/fsudouest/blablafit/Ui/Fragments/MessagesFragment.kt
+++ app/src/main/java/com/example/fsudouest/blablafit/Ui/Fragments/MessagesFragment.kt
@@ -6,11 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.fsudouest.blablafit.R
+import com.example.fsudouest.blablafit.databinding.FragmentMessagesBinding
 
 
 /**
- * A simple [Fragment] subclass.
+ * A Fragment that displays the list of the user's messages with other blablafit users
  */
 class MessagesFragment : Fragment() {
 
@@ -18,7 +20,8 @@ class MessagesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false)
+        val binding: FragmentMessagesBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_messages,container,false)
+        return binding.root
     }
 
-}// Required empty public constructor
+}
