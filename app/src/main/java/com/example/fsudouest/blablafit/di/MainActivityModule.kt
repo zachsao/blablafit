ee182e767a16c722c2030fcd372fbc671b5eb892
zacharia <zakaria.sao@gmail.com>
2019-02-21 22:38:37 +0100

    Update MainActivityModule.kt

--- app/src/main/java/com/example/fsudouest/blablafit/di/MainActivityModule.kt
+++ app/src/main/java/com/example/fsudouest/blablafit/di/MainActivityModule.kt
@@ -1,7 +1,11 @@
 package com.example.fsudouest.blablafit.di
 
+import com.example.fsudouest.blablafit.MainActivity
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
-class MainActivityModule {
+abstract class MainActivityModule {
+    @ContributesAndroidInjector(modules = [FragmentBuildersModule::class])
+    abstract fun contributeMainActivity(): MainActivity
 }
