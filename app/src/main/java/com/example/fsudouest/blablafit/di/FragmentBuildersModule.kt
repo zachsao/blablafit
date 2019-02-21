8d83217a3bb66d3d783386b1024e1d64c10ba88f
zacharia <zakaria.sao@gmail.com>
2019-02-21 22:37:49 +0100

    Update FragmentBuildersModule.kt

--- app/src/main/java/com/example/fsudouest/blablafit/di/FragmentBuildersModule.kt
+++ app/src/main/java/com/example/fsudouest/blablafit/di/FragmentBuildersModule.kt
@@ -1,4 +1,23 @@
 package com.example.fsudouest.blablafit.di
 
-class FragmentBuildersModule {
+import com.example.fsudouest.blablafit.Ui.Fragments.MessagesFragment
+import com.example.fsudouest.blablafit.Ui.Fragments.MyProfileFragment
+import com.example.fsudouest.blablafit.Ui.Fragments.SeancesFragment
+import com.example.fsudouest.blablafit.Ui.Fragments.TrouverUneSeanceFragment
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class FragmentBuildersModule {
+    @ContributesAndroidInjector
+    abstract fun contributeMessagesFragment(): MessagesFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeMyProfileFragment(): MyProfileFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeSeancesFragment(): SeancesFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeTrouverUneSeanceFragment(): TrouverUneSeanceFragment
 }
