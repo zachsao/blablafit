24f3d92ea868bded64c13c19ca2adf4b23d78960
zacharia <zakaria.sao@gmail.com>
2019-02-21 22:40:21 +0100

    Update BlablaFitApp.kt

--- app/src/main/java/com/example/fsudouest/blablafit/BlablaFitApp.kt
+++ app/src/main/java/com/example/fsudouest/blablafit/BlablaFitApp.kt
@@ -1,6 +1,20 @@
 package com.example.fsudouest.blablafit
 
+import android.app.Activity
 import android.app.Application
+import com.example.fsudouest.blablafit.di.AppInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
-class BlablaFitApp : Application() {
+class BlablaFitApp : Application(), HasActivityInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    override fun onCreate() {
+        super.onCreate()
+        AppInjector.init(this)
+    }
+
+    override fun activityInjector() = dispatchingAndroidInjector
 }
