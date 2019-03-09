3be6be03660042dd9b75b32a12fbf223c70f8e13
zacharia <zakaria.sao@gmail.com>
2019-03-09 19:21:52 +0100

    create date and time selection screen

--- app/src/main/java/com/example/fsudouest/blablafit/Ui/Fragments/AddDateDurationFragment.kt
+++ app/src/main/java/com/example/fsudouest/blablafit/Ui/Fragments/AddDateDurationFragment.kt
@@ -7,20 +7,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
-import com.example.fsudouest.blablafit.databinding.FragmentAddDescriptionBinding
+
 import com.example.fsudouest.blablafit.R
+import com.example.fsudouest.blablafit.databinding.FragmentAddDateDurationBinding
 
 
-class AddDescriptionFragment : Fragment() {
+class AddDateDurationFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentAddDescriptionBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_add_description, container, false)
-        binding.nextStepButton.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_addDescriptionFragment_to_addDateDurationFragment)
-        }
+        val binding: FragmentAddDateDurationBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_add_date_duration, container, false)
         return binding.root
     }
 
