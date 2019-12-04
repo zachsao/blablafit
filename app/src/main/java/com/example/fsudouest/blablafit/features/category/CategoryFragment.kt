1ff1baad14a0250d9603d5286c84d0e6032964a0
Zach Sao <zakaria.sao@gmail.com>
2019-12-03 21:33:02 -0500

    set clickListener in category fragment list item

--- app/src/main/java/com/example/fsudouest/blablafit/features/category/CategoryFragment.kt
+++ app/src/main/java/com/example/fsudouest/blablafit/features/category/CategoryFragment.kt
@@ -2,21 +2,23 @@ package com.example.fsudouest.blablafit.features.category
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-
 import com.example.fsudouest.blablafit.R
 import com.example.fsudouest.blablafit.databinding.FragmentCategoryBinding
 import com.example.fsudouest.blablafit.di.Injectable
+import com.example.fsudouest.blablafit.features.nearby.ui.CategoryViewItem
+import com.example.fsudouest.blablafit.features.workoutDetails.DetailsSeanceActivity
 import com.example.fsudouest.blablafit.utils.ViewModelFactory
 import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.Section
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import org.jetbrains.anko.support.v4.startActivity
 import javax.inject.Inject
 
 class CategoryFragment : Fragment(), Injectable {
@@ -38,13 +40,16 @@ class CategoryFragment : Fragment(), Injectable {
         initList()
         viewModel.getWorkouts(args.title)
 
-
-
         return binding.root
     }
 
     private fun initList() {
-        binding.recyclerView.adapter = GroupAdapter<GroupieViewHolder>()
+        binding.recyclerView.adapter = GroupAdapter<GroupieViewHolder>().apply {
+            setOnItemClickListener { item, _ ->
+                item as WorkoutViewItem
+                findNavController().navigate(CategoryFragmentDirections.actionCategoryFragmentToDetailsSeanceActivity(item.seance.id))
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/fsudouest/blablafit/features/category/WorkoutViewItem.kt
+++ app/src/main/java/com/example/fsudouest/blablafit/features/category/WorkoutViewItem.kt
@@ -5,7 +5,7 @@ import com.example.fsudouest.blablafit.databinding.SeanceItem2Binding
 import com.example.fsudouest.blablafit.model.Seance
 import com.xwray.groupie.databinding.BindableItem
 
-class WorkoutViewItem(private val seance: Seance) : BindableItem<SeanceItem2Binding>() {
+class WorkoutViewItem(val seance: Seance) : BindableItem<SeanceItem2Binding>() {
     override fun bind(viewBinding: SeanceItem2Binding, position: Int) {
         viewBinding.seance = seance
     }
